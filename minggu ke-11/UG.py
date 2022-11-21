6a82c9e5c0dd529d8ef7af0c491fc27b90b2f0c3
VitoKa29 <nathanael.vito@ti.ukdw.ac.id>
2022-11-21 19:12:29 +0700

    Update UG.py

--- minggu ke-11/UG.py
+++ minggu ke-11/UG.py
@@ -6,28 +6,22 @@ class RakObat:
     def _getHash(self, key):
         hash = 0
         for char in str(key):
-            hash += ord(char) # mendapatkan nilai ASCII
+            hash += ord(char)
         return hash % self.size
 
     def _probing(self, key):
         for index in range(self.size):
-            # probeHash = (self._getHash(key)+index) % self.size
             probeHash = self._linearProbing(key, index)
-            # valid bila index adalah None atau ber-flag deleted
             if (self.map[probeHash] is None) or (self.map[probeHash] == 'deleted'):
                 return probeHash
 
         return None
 
-    # melakukan linear probing
     def _linearProbing(self, key, index):
         return (self._getHash(key)+index) % self.size
 
-    # menambahkan key pada hash table
     def tambahObat(self, key, value):
-        # periksa apakah key_hash sudah terpakai
         key_hash = self._getHash(key)
-        # buat pasangan key value
         key_value = [key, value]
 
         if self.map[key_hash] is None:
@@ -46,9 +40,7 @@ class RakObat:
         key_hash = self._getHash(key)
         if (self.map[key_hash] is not None) and (self.map[key_hash] != 'deleted'):
             for index in range(self.size):
-                #mencari dengan melakukan probing
                 key_hash = self._linearProbing(key, index)
-                # periksa apakah key adalah data yg akan dihapus
                 if(self.map[key_hash][0][0] == key):
                     return self.map[key_hash][0][1]
 
@@ -60,9 +52,7 @@ class RakObat:
         if self.map[key_hash] is None:
             return False
         for index in range(self.size):
-            #menghapus dengan melakukan linear probing
             key_hash = self._linearProbing(key, index)
-            # periksa apakah key adalah data yg akan dihapus
             if(self.map[key_hash][0][0] == key):
                 print('obat', key, 'diambil dari rak')
                 self.map[key_hash] = "deleted"
